cd237998cfb7f4a60fd65abc56193cf8ac7b9b6b
Gabi <bellome.gabriel@gmail.com>
2022-11-21 14:12:03 -0300

    Inserté la foto de perfil en el sidebar

--- src/Components/SideBar/Components/ImagenPerfil.jsx
+++ src/Components/SideBar/Components/ImagenPerfil.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import FotoPerfil from '../../../Assets/img/Perfil BN.jpg'
 import './../Styles/ImagenPerfilStyle.css'
 
 const ImagenPerfil = () => {
   return (
     <div id='ImagenPerfil'>
-      <img src="https://robohash.org/user1" alt="Imagen Perfil" />
+      <img src={FotoPerfil} alt="Imagen Perfil" />
       <div>
         <h1>Gabriel Bellome</h1>
       </div>
