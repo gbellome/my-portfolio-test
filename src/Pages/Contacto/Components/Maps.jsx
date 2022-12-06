53f977bfc6835d3e082df475bcdbcef300dae112
Gabi <bellome.gabriel@gmail.com>
2022-12-05 22:54:04 -0300

    Actualizado el mapa

--- src/Pages/Contacto/Components/Maps.jsx
+++ src/Pages/Contacto/Components/Maps.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Maps = () => {
   return (
     <iframe 
-    src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d13136.590722976262!2d-58.565810549999995!3d-34.60042695!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2sar!4v1670284717677!5m2!1ses!2sar"
+    src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d13138.59641428686!2d-58.577998507763674!3d-34.58774430720785!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses-419!2sar!4v1670291591094!5m2!1ses-419!2sar"
     allowfullscreen="" 
     loading="lazy"
     style={{'border': '0px'}}
@@ -14,3 +14,7 @@ const Maps = () => {
 }
 
 export default Maps
+
+
+
+
